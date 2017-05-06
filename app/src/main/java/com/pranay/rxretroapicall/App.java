00b66befa254f859e889d546b85852816ab85b86
pranaypatel512 <www.pranay1337.com@gmail.com>
2017-05-06 16:31:11 +0530

    initial commit

---
+++ app/src/main/java/com/pranay/rxretroapicall/App.java
@@ -0,0 +1,15 @@
+package com.pranay.rxretroapicall;
+
+import android.app.Application;
+
+/**
+ * Created by Pranay.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
